d2b737ebe09eb2d2f09cfa2cdcabbe40adadcb83
uchrissd <uchrissd@protonmail.com>
2020-02-18 08:07:02 -0800

    updated api file with axios call, added createreactapp serviceworker file, removed pages folder becuase i dont need them, added a app style css file

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
+const BASEURL = "https://randomuser.me/api/?results=";
 
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
+  search: function(numResults) {
+    return axios.get(BASEURL + numResults);
   }
 };
